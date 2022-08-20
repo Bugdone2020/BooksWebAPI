9245a30aee9c7053889651d4857597a7e80bc60e
Bugdone2020 <bohdankozlov@gmail.com>
2022-08-20 20:03:15 +0300

    Implemented MLA

---
+++ BooksWebAPI_DAL/EFCoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BooksWebAPI_DAL
+{
+    public class EFCoreDbContext: DbContext
+    {
+        public DbSet<Book> Books { get; set;}
+
+        public EFCoreDbContext(DbContextOptions<EFCoreDbContext> options) : base(options)
+        { 
+            Database.EnsureCreated();
+        }
+    }
+}
