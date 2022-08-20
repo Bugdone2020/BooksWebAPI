ae2b247fe0cec79edd64cc4251c1d528c60a2fff
Bugdone2020 <bohdankozlov@gmail.com>
2022-08-20 20:24:10 +0300

    Implemented client logic

---
+++ BooksWebAPI_BL/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using BooksWebAPI_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksWebAPI_BL
+{
+    public class ClientService : IClientService
+    {
+        public bool RentABook(BooksService book, Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReturnABook(Book book, Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
